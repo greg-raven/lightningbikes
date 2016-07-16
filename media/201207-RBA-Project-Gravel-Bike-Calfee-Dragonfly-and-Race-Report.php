4359f7e98b1758a9b4e4385a3aa25ba3cd34ecd6
Greg Raven <greg@corax.org>
2016-07-16 05:20:06 -0700

    reorganization

---
+++ media/201207-RBA-Project-Gravel-Bike-Calfee-Dragonfly-and-Race-Report.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+	<title>Bike test: Project Gravel Reborn (Road Bike Action, July 2012)</title>
+<!-- #bbinclude "lcd/metatags.incl" -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+	<link href="https://fonts.googleapis.com/css?family=Archivo+Narrow:700,700italic" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="/css/lcd.css" type="text/css">
+<!-- end lcd/metatags.incl -->
+<!-- end bbinclude -->
+</head>
+<body>
+<!-- #bbinclude "lcd/content-container.incl" -->
+<div class="container-fluid">
+	<div class="row">
+		<article class="col-sm-10 col-sm-push-2">
+<!-- end lcd/content-container.incl -->
+<!-- end bbinclude -->
+			<h1>
+				BIKETEST: Project Gravel Reborn
+			</h1>
+			<p class="lead">
+				What does it take to build the ultimate gravel bike?
+			</p>
+			<p>
+				Regular readers will recall our July issue where we featured a test of a Specialized Crux cyclocross bike that began a process of morphing it into something different than a &rsquo;cross-specific race bike. Although we did race the bike, we saw more potential in the Crux than just clearing barriers, and that was based on all the talk of the latest trend in drop-bar usage, the world of what&rsquo;s become popularly known as &ldquo;gravel bikes.&rdquo; And so the Crux became our first gravel project bike.
+			</p>
+			<p>
+				However, about the same time we started the Crux project bike, we caught wind of a particularly grueling on/off road race that was six months off in the future. The Crusher in the Tushar is a 69-mile race held in Beaver, Utah, that is a mix between pavement and dirt roads. The dirt sections are rutted, loose mountain roads with golf-ball-size rocks sprinkled on them to keep things interesting. Oh, and lest we forget, it also boasts 10,500 feet of climbing. (That was the part that caught Neil&rsquo;s attention.) And while the Crux would be a great bike for the event, we figured the race would also be a good opportunity to build our own version of the ultimate gravel bike. And so RBA&rsquo;s &ldquo;Project Gravel Bike&rdquo; was born.
+			</p>
+			<p>
+				The intent of the project was twofold: Build a gravel bike to show how versatile a road bike can be across different terrain while utilizing some of the latest technology we could find. Secondly, and perhaps most importantly, build a bike that Neil could win on.
+			</p>
+			<p>
+				[&hellip;]
+			</p>
+			<p>
+				Crank duties went to the California-made Lightning Cranks. Their hollow, tubular carbon arms and CNC-machined aluminum spider make them light, but they&rsquo;re also compatible with nearly every bottom bracket on the market.
+			</p>
+			<figure>
+				<img src="/images/media/201207-RBA-Project-Gravel.jpg" width="600" height="612" alt="RBA-Project-Gravel" class="img-thumbnail">
+			</figure>
+			<p>
+				[&hellip;]
+			</p>
+			<p>
+				California-made Lightning cranks were the preferred choice due to their stiffness and low weight. The cranks are also compatible with most of the bottom bracket designs on the market, including the PressFit 30 used on Calfee&rsquo;s exquisite bottom bracket lug.
+			</p>
+			<p>
+				[&hellip;]
+			</p>
+			<iframe src="/media/201207-RBA-Project-Gravel-Bike-Calfee-Dragonfly-and-Race-Report.pdf" class="pdf"> </iframe>
+			<p>
+				Source: <a href="http://roadbikeaction.com/">Road Bike Action</a>, July 2012
+			</p>
+<!-- #bbinclude "lcd/sidebar.incl" -->
+		</article>
+		<div class="col-sm-2 col-sm-pull-10">
+			<nav class="inside">
+				<a href="/index.html" title="Return to the home page">Home</a> <a href="/introduction.html" title="Introduction" id="intro">Introduction</a> <a href="/recumbents.html" title="Bicycles" id="recumbents">Recumbents</a> <a href="/handcycles/index.html" title="Hand cycles" id="handcycles">HandCycles</a>
+<!--<a href="/e-lightning/index.html" title="Bicycles">E-Lightning</a>-->
+				<a href="/cranks/index.html" title="cranksets" id="cranksets">Cranksets</a> <a href="/cranks/crank-nav.html" title="crank parts" id="crankparts">Crank parts</a> <a href="/accessories/index.html" title="Accessories" id="accessories">Accessories</a> <a href="/pricelist.html" title="Prices" id="prices">Price list</a> <a href="/dealers/index.html" title="Dealers" id="dealers">Dealers</a> <a href="/howtobuy.html" title="How to buy" id="buying">How to buy</a> <a href="/media/index.html" title="Media" id="media">Media</a> <a href="/riders/racing.html" title="Racing" id="racing">Racing</a> <a href="/gallery/index.html" title="Photo gallery" id="photos">Photos</a> <a href="/videos/index.html" title="Videos" id="videos">Videos</a> <a href="/faq.html" title="Frequently asked questions" id="faq">F.A.Q.</a> <a href="/tips/index.html" title="Tech Tips" id="tips">Tech Tips</a>
+<!--<a href="http://www.lightningriders.com/" title="Lightning Riders">Fan Club</a> <a href="http://www.lightningriders.com/forum/" title="Lightning Riders forum">Forum</a>-->
+				<a href="/company.html" title="About Lightning Cycle Dynamics" id="about">About us</a> <a href="/contact.html" title="Contact Lightning Cycle Dynamics" id="contact">Contact us</a> <a href="/search.html" title="Search Lightning Cycle Dynamics" id="search">Search</a>
+			</nav>
+		</div>
+<!-- /.col-sm-2 col-sm-pull-10 -->
+	</div>
+<!-- /.row -->
+<!-- end lcd/sidebar.incl -->
+<!-- end bbinclude -->
+<!-- #bbinclude "lcd/address.incl" -->
+	<div class="row">
+		<div class="col-sm-12">
+			<footer class="address">
+				<hr>
+				<p class="text-center">
+					Copyright &copy; Lightning Cycle Dynamics, 1977&ndash;2016
+				</p>
+			</footer>
+		</div>
+<!-- /.col-sm-12 -->
+	</div>
+<!-- /.row -->
+</div>
+<!-- /.container-fluid -->
+<!-- end lcd/address.incl -->
+<!-- end bbinclude -->
+<script>
+	document.getElementById("media").className = "active";
+</script>
+<!-- #bbinclude "lcd/analytics.incl" -->
+<script type="text/javascript">
+	var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-37984504-1']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();
+</script>
+<!-- end lcd/analytics.incl -->
+<!-- end bbinclude -->
+</body>
+</html>
