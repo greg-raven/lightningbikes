d29daa4904f03b1f8db1c89773068fb172d85421
Greg Raven <greg@corax.org>
2016-08-12 13:38:24 -0700

    Phantom II review 2015 BROL

---
+++ media/20150104-brol.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+	<meta charset="utf-8">
+	<title>Review: Lightning Phantom II (&rsquo;BentRider Online, 2015)</title>
+<!-- #bbinclude "lcd/metatags.incl" -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+	<link href="https://fonts.googleapis.com/css?family=Archivo+Narrow:700,700italic" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="/css/lcd.css" type="text/css">
+<!-- end lcd/metatags.incl -->
+<!-- end bbinclude -->
+</head>
+<body>
+<!-- #bbinclude "lcd/content-container.incl" -->
+<div class="container-fluid">
+	<div class="row">
+		<article class="col-sm-10 col-sm-push-2">
+<!-- end lcd/content-container.incl -->
+<!-- end bbinclude -->
+			<p class="lead">
+				Review
+			</p>
+			<h1>
+				Lightning Phantom II
+			</h1>
+			<p>
+				by Bryan Ball<br>
+				Managing Editor, &rsquo;BentRider Online<br>
+				January 4, 2015<br>
+			</p>
+			<p>
+				Lightning Cycle Dynamics is best known for their much beloved and enduring P-38. However, their less-expensive model also has a very loyal following. Much to the relief of those fans, the Phantom II has returned after a brief production hiatus. It&rsquo;s also hit the ground running with a host of improvements.
+			</p>
+			<p>
+				The frame is stronger and a bit lighter, the seat has been updated slightly and the tilting steering column is now standard. Wheels and components have also been brought up to date.
+			</p>
+			<figure>
+				<img src="/images/media/20150104-phantom-II.jpg" width="800" height="530" alt="20150104 Phantom II review" class="img-thumbnail">
+			</figure>
+			<p>
+				The 2015 Phantom II comes with a mostly Shimano Deore 27-speed drivetrain using a Sugino crankset and Tektro V-Brakes. The new 559 rear and 406 front rims are slightly aero-shaped ARDC units with 24 spokes in the front and 28 in the rear. The Phantom II rolls on Primo Comet tires. The total package weighs a very reasonable 28.5 pounds for the large frame and retails for $1,795.
+			</p>
+			<p>
+				Build quality also seems to have stepped up a notch. The frame&rsquo;s welds and things like the seat and idlers appear to me a bit nicer than on previous Phantoms.
+			</p>
+			<p>
+				Lightning delivered my test Phantom II with an optional spring loaded front idler that makes adjustment easier for dealers. It&rsquo;s a bit unsightly but works well and runs quiet. It can be replaced with a single idler without any chain rub for most riders.
+			</p>
+			<p>
+				One advantage that the Phantom II has always had over the pricier P-38 is its adjustability. The seat angle moves between 50 and 65 degrees and the boom can telescope to fit riders between 5&prime;7&Prime; and 6&prime; 6&Prime; or so on the Large model I reviewed. There is a smaller frame with a 16&Prime; front wheel for shorter riders.
+			</p>
+			<p>
+				I&rsquo;ve always found Lightnings to be very comfortable bikes and the Phantom II was no exception. The P-38 is more closed and has a sort of &ldquo;love it or hate it&rdquo; riding position but the Phantom II is much more accommodating. Lightning&rsquo;s improved seat is also quite cushy and breathable. It&rsquo;s also much easier for riders with shorter legs to get their feet flat on the ground than it was in the past, but the smaller frame size with its smaller front wheel will still be needed by some people for sure.
+			</p>
+			<p>
+				Lighting&rsquo;s locking stem isn&rsquo;t as pretty as some other folding steering risers but it does work exceeding well and allows for a wide variety of hand positions. The Phantom II now comes with trigger shifters which is a bit rare on a recumbent but I really like them. They fit the bike&rsquo;s ergonomics very well.
+			</p>
+			<p>
+				The Phantom II&rsquo;s geometry and overall design are a bit old school. The wheelbase is quite short when compared to something like a Bacchetta Giro or RANS Rocket but I really like the way it handles. The low speed handing is very direct but by no means twitchy. Once you&rsquo;re used to it, very tight parking lot or bike path turns are surprisingly easy.
+			</p>
+			<p>
+				Although the Phantom II is quite nimble at low speeds, the high speed handing doesn&rsquo;t seem to suffer. It&rsquo;s &ldquo;happy place&rdquo; seems to be somewhere in that 15-30 mph range where the bike will reside most of the time. At those speeds, the Phantom II was almost hands-off stable.
+			</p>
+			<p>
+				Like most Lightning&rsquo;s the Phantom II is surprisingly fast. It&rsquo;s not particularly aerodynamic or light but something about that Lightning seating position seems to eek every bit of speed out of a design. It doesn&rsquo;t have quite that magic &ldquo;zing&rdquo; of its much more expensive P-38 big brother but it&rsquo;s close. The Phantom II is definitely every bit as fast any other bike in its class.
+			</p>
+			<p>
+				I spent most of my time riding the Phantom II on my local bike paths cruising along at 15-20 mph looking at the sites. It&rsquo;s a fantastic bike for that. It&rsquo;s got a very smooth ride, it&rsquo;s comfortable, it&rsquo;s quiet and the upright seating position gives you a good view of what&rsquo;s around you.
+			</p>
+			<p>
+				The Phantom II can also easily accept fenders, fatter tires, a rear rack, a kickstand and almost any other accessory you want to throw at it. It&rsquo;s very easy to customize this bike for fast cruising, touring, commuting or almost any other purpose you can think of. There&rsquo;s even an outrigger quad kit available if you&rsquo;re worried about your balance.
+			</p>
+			<p>
+				Of course the Phantom II isn&rsquo;t perfect in every way. The graphics are getting a bit long in the tooth. The stock idler doesn&rsquo;t look very refined either.
+			</p>
+			<p>
+				Those caveats aside, I&rsquo;m very glad to see the Phantom II back. It&rsquo;s comfortable, easy to live with, reasonably priced and quick enough to satisfy most riders.
+			</p>
+			<p>
+				<b>Lightning Phantom II</b>
+			</p>
+			<p>
+				Highs &mdash; Comfortable, Versatile, Pretty quick
+			</p>
+			<p>
+				Lows &mdash; May be a bit old school for some
+			</p>
+			<p>
+				MSRP &mdash; $1,795
+			</p>
+			<p>
+				Source: <a href="http://www.bentrideronline.com/?p=9922">&rsquo;BentRider Online</a>
+			</p>
+<!-- #bbinclude "lcd/sidebar.incl" -->
+		</article>
+		<div class="col-sm-2 col-sm-pull-10">
+			<nav class="inside">
+				<a href="/index.html" title="Return to the home page">Home</a> <a href="/introduction.html" title="Introduction" id="intro">Introduction</a> <a href="/recumbents.html" title="Bicycles" id="recumbents">Recumbents</a> <a href="/handcycles/index.html" title="Hand cycles" id="handcycles">HandCycles</a>
+<!--<a href="/e-lightning/index.html" title="Bicycles">E-Lightning</a>-->
+				<a href="/cranks/index.html" title="cranksets" id="cranksets">Cranksets</a> <a href="/cranks/crank-nav.html" title="crank parts" id="crankparts">Crank parts</a> <a href="/accessories/index.html" title="Accessories" id="accessories">Accessories</a> <a href="/pricelist.html" title="Prices" id="prices">Price list</a> <a href="/dealers/index.html" title="Dealers" id="dealers">Dealers</a> <a href="/howtobuy.html" title="How to buy" id="buying">How to buy</a> <a href="/media/index.html" title="Media" id="media">Media</a> <a href="/racing/index.html" title="Racing" id="racing">Racing</a><a href="/riders/index.html" title="Riders" id="riders">Riders</a> <a href="/gallery/index.html" title="Photo gallery" id="photos">Photos</a> <a href="/videos/index.html" title="Videos" id="videos">Videos</a> <a href="/faq.html" title="Frequently asked questions" id="faq">F.A.Q.</a> <a href="/tips/index.html" title="Tech Tips" id="tips">Tech Tips</a>
+<!--<a href="http://www.lightningriders.com/" title="Lightning Riders">Fan Club</a> <a href="http://www.lightningriders.com/forum/" title="Lightning Riders forum">Forum</a>-->
+				<a href="/company.html" title="About Lightning Cycle Dynamics" id="about">About us</a>
+<!-- <a href="/contact.html" title="Contact Lightning Cycle Dynamics" id="contact">Contact us</a> -->
+				<a href="/search.html" title="Search Lightning Cycle Dynamics" id="search">Search</a>
+			</nav>
+		</div>
+<!-- /.col-sm-2 col-sm-pull-10 -->
+	</div>
+<!-- /.row -->
+<!-- end lcd/sidebar.incl -->
+<!-- end bbinclude -->
+<!-- #bbinclude "lcd/address.incl" -->
+	<div class="row">
+		<div class="col-sm-12">
+			<footer class="address">
+				<hr>
+				<p class="text-center">
+					Copyright &copy; <a href="/company.html">Lightning Cycle Dynamics</a>, 1977&ndash;2016
+				</p>
+			</footer>
+		</div>
+<!-- /.col-sm-12 -->
+	</div>
+<!-- /.row -->
+</div>
+<!-- /.container-fluid -->
+<!-- end lcd/address.incl -->
+<!-- end bbinclude -->
+<script>
+	document.getElementById("media").className = "active";
+</script>
+<!-- #bbinclude "lcd/analytics.incl" -->
+<script type="text/javascript">
+	var _gaq = _gaq || []; _gaq.push(['_setAccount', 'UA-37984504-1']); _gaq.push(['_trackPageview']); (function() { var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); })();
+</script>
+<!-- end lcd/analytics.incl -->
+<!-- end bbinclude -->
+</body>
+</html>
