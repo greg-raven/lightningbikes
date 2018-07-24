7afbbbf22219b77ebfc4cc914439ae19c362092c
Greg Raven <greg@corax.org>
2018-07-24 16:10:27 -0700

    change menu link to new store location

--- _assets/js/currentyear.js
+++ _assets/js/currentyear.js
@@ -3,4 +3,6 @@ var currentTime = new Date();
 // returns the year (four digits)
 var year = currentTime.getFullYear();
 // insert current year into document
+// first create span element with
+// id of currentyear
 document.getElementById("currentyear").innerHTML = year;
