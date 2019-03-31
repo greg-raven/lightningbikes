77f85ab1fb840fa64870aeeea8f87c5786a1d8fd
Greg Raven <gregraven@mac.com>
2019-03-31 07:35:04 -0700

    update https redirection

---
+++ _assets/js/switchProtocol-min.js
@@ -0,0 +1,2 @@
+"https:"!==document.location.protocol&&(document.location.href="https://www.lightningbikes.com"+document.location.pathname);
+//# sourceMappingURL=switchProtocol-min.js.map
---
+++ _assets/js/switchProtocol.js
@@ -0,0 +1,3 @@
+if (document.location.protocol !== "https:") {
+    document.location.href = "https://www.lightningbikes.com" + document.location.pathname;
+}
