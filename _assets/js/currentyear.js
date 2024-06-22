1e3f30a0eb6f89f1b62f88905563eb90a6dd106f
Greg Raven <gregraven@mac.com>
2024-06-22 08:20:54 -0700

    replace single quotes with double

--- _assets/js/currentyear.js
+++ _assets/js/currentyear.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
     // This function runs after the DOM is fully loaded
     var currentTime = new Date();
     var year = currentTime.getFullYear();
-    var currentYearElement = document.getElementById("currentyear");
+    var currentYearElement = document.getElementById("currentYear");
 
     // Check if element exists before setting innerHTML
     if (currentYearElement) {
         currentYearElement.innerHTML = year;
     } else {
-        console.error("Element with id 'currentyear' not found.");
+        console.error("Element with id 'currentYear' not found.");
     }
 });
